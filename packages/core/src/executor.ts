e4cae7b5369dbe543ff2eca9c9c02afd408f722d
mihirjayavant <mihir.jayavant2@gmail.com>
2019-08-21 19:20:04 +0530

    refractor and added dialog executor

---
+++ packages/core/src/executor.ts
@@ -0,0 +1,33 @@
+import { IStore, IActivatedRoute, IActivity, IDialogAction, dialogFn } from './interfaces'
+import { findRoute } from './router'
+
+export function storeExecutor<T>(
+  store: IStore<T>,
+  stateSlice: Pick<T, keyof T>,
+  activatedRoute: IActivatedRoute
+): IStore<T> {
+  return {
+    ...store,
+    data: { ...store.data, ...stateSlice },
+    activatedRoute
+  }
+}
+
+export function routeExecutor<T>(store: IStore<T>) {
+  const active = store.activatedRoute
+  return findRoute([...active.parent, active.path], store.routes)
+}
+
+export function dialogExecutor<T>(
+  store: IStore<T>,
+  activity: IActivity,
+  dialog: dialogFn<T>
+): [IStore<T>, IDialogAction<T>] {
+  const { activatedRoute } = store
+  const routes = store.rawRoutes
+
+  const response = dialog(store.data, activity, { activatedRoute, routes })
+  const newStore = storeExecutor(store, response.state, response.navigateTo || store.startRoute)
+
+  return [newStore, response]
+}
